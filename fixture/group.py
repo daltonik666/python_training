efcb20ef6485e61b8c4c6a9e697c590198c1391c
daltonik <daltonik666@mail.ru>
2020-08-21 12:31:20 +0300

    Разделение теста на модифицирование группы (свой от урока)

--- fixture/group.py
+++ fixture/group.py
@@ -36,19 +36,23 @@ class GroupHelper:
         driver.find_element_by_name("selected[]").click()
         # init group edit
         driver.find_element_by_name("edit").click()
-        # fill group form
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(group.name)
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(group.header)
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group edit
         driver.find_element_by_name("update").click()
         self.return_to_groups_page()
+
+    def fill_group_form(self, group):
+        driver = self.app.driver
+        self.type("group_name", group.name)
+        self.type("group_header", group.header)
+        self.type("group_footer", group.footer)
+
+    def type(self, field_name, text) :
+        driver = self.app.driver
+        if text is not None:
+            driver.find_element_by_name(field_name).click()
+            driver.find_element_by_name(field_name).clear()
+            driver.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_group(self):
         driver = self.app.driver
@@ -67,9 +71,12 @@ class GroupHelper:
         self.open_groups_page()
         self.select_first_group()
         # open modification form
+        driver.find_element_by_name("edit").click()
         # fill group form
-        #
-
+        self.fill_group_form(new_group_data)
+        # submit modification
+        driver.find_element_by_name("update").click()
+        self.return_to_groups_page()
 
 
     def return_to_groups_page(self):
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ -*- coding: utf-8 -*-
 from model.group import Group
 
 def test_edit_group(app):
@@ -6,13 +6,3 @@ def test_edit_group(app):
     app.group.edit(Group(name="groupname1", header="headre1", footer="footer1"))
     app.session.logout()
 
-def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="New group"))
-    app.session.logout()
-
-
-def test_modify_group_header(app):
-    app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(header="New header"))
-    app.session.logout()
