dd5ddb153e55d016560ab8e0b59fa624a623880f
daltonik <daltonik666@mail.ru>
2020-08-21 13:36:52 +0300

    Добавление условий повторного логина

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,5 +4,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="groupname", header="headre", footer="footer"))
 
+
 def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
