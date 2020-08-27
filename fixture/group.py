67070a5cc1d7a45acf38d54ff0fd0ab4e8407aec
daltonik <daltonik666@mail.ru>
2020-08-28 01:04:55 +0300

    Сравнение списков групп после добавления\удаления\редактирования

--- fixture/group.py
+++ fixture/group.py
@@ -1,5 +1,5 @@
 
-
+from model.group import Group
 
 class GroupHelper:
 
@@ -81,4 +81,12 @@ class GroupHelper:
         self.open_groups_page()
         return len(driver.find_elements_by_name("selected[]"))
 
-
+    def get_group_list(self):
+        driver = self.app.driver
+        self.open_groups_page()
+        groups = []
+        for element in driver.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
