953219959c236917f6aab6cbe11512b14a695086
daltonik <daltonik666@mail.ru>
2020-08-28 01:07:16 +0300

    рефакториенг вызова драйвера

--- fixture/application.py
+++ fixture/application.py
@@ -9,9 +9,6 @@ class Application:
 
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
